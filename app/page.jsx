5ccf32a5dc855a9a921d1f9ae45266a3dc3081fe
unknown <ajincraju@gmail.com>
2025-07-11 21:55:00 +0530

    Fixed routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,6 @@
-'use client'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Page () {
-  const router = useRouter()
-  useEffect(() => {
-    router.replace('/address')
-  }, [router])
+  redirect('/delivery')
   return null
 }
